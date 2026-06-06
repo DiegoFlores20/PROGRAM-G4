9be7d8207631b5a84d0336a2432782e080dd3c96
DiegoFlores20 <floresdiego2006a@gmail.com>
2026-06-06 18:26:38 -0500

    commit 4 Flores Diego
    Agregar opciones 1 a 5 del menú

--- src/jiji/Jiji.java
+++ src/jiji/Jiji.java
@@ -9,6 +9,11 @@ public class Jiji {
         
         do {
             System.out.println("\n===== MENU PRINCIPAL =====");
+            System.out.println("1. Contraseña ilimitada");
+            System.out.println("2. Positivos, suma y promedio");
+            System.out.println("3. Validar nota");
+            System.out.println("4. Cajero automático");
+            System.out.println("5. Pares e impares");
         } while (opcion != 16); 
     }
     
