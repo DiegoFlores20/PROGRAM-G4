8019437eef5d31a4bb4078f08fbd8ab0b0a5b204
DiegoFlores20 <floresdiego2006a@gmail.com>
2026-06-06 18:30:29 -0500

    commit 5 Flores Diego
    Agregar opciones 6 a 10 del menú

--- src/jiji/Jiji.java
+++ src/jiji/Jiji.java
@@ -14,6 +14,11 @@ public class Jiji {
             System.out.println("3. Validar nota");
             System.out.println("4. Cajero automático");
             System.out.println("5. Pares e impares");
+            System.out.println("6. Ventas diarias");
+            System.out.println("7. Números hasta negativo");
+            System.out.println("8. Tabla de multiplicar");
+            System.out.println("9. Factorial");
+            System.out.println("10. Positivos, negativos y ceros");
         } while (opcion != 16); 
     }
     
