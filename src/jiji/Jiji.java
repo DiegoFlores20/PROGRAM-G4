a2056db44da8e2d39d7e7c31d36de4dcfa6df348
DiegoFlores20 <floresdiego2006a@gmail.com>
2026-06-06 00:20:45 -0500

    commitprueba
    flores diego (prueba de q la rama funciona)

--- src/jiji/Jiji.java
+++ src/jiji/Jiji.java
@@ -4,6 +4,7 @@ public class Jiji {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola mundo");
+        System.out.println("hola mundo2");
     }
     
 }
