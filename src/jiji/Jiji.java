c8c9d281276733f5bab1e3cef5871982b6e3bf9e
DiegoFlores20 <floresdiego2006a@gmail.com>
2026-06-06 18:22:39 -0500

    commit 2 Flores Diego
    Agregar ciclo principal del menú

--- src/jiji/Jiji.java
+++ src/jiji/Jiji.java
@@ -2,9 +2,13 @@ package jiji;
 import java.util.Scanner;
 public class Jiji {
     static Scanner leer = new Scanner(System.in);
+    
     public static void main(String[] args) {
+        
         int opcion;
-
+        
+        do {
+        } while (opcion != 16); 
     }
     
 }
